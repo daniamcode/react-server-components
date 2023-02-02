dd35f494db5a865284b9f59b4a219ca148aa3619
Dani Alcala <daniamcode@gmail.com>
2023-02-02 19:03:38 +0100

    fake backend and await in React Server Component

--- nextjs-version/src/app/page.tsx
+++ nextjs-version/src/app/page.tsx
@@ -1,5 +1,20 @@
-export default function Home() {
+// to fake the backend
+const fetchDescription = () =>
+  new Promise((resolve) =>
+    setTimeout(() => resolve("Product information ready for SEO"), 250)
+  );
+
+// with React Server Components (by default in Nextjs 13), we can await inside components
+export default async function Home() {
+  const description = await fetchDescription()
   return (
-    <div></div>
+    <>
+      <header>Header</header>
+      
+      <h2>Product description</h2>
+      {/* <p>{description}</p> */}
+      
+      <footer>Footer</footer>
+    </>
   )
 }
